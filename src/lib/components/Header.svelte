570d385a5b674eb36ba1ebde2b0d6bee5edc92a9
csslick <digitone@naver.com>
2024-05-25 23:23:25 +0900

    svelte-diary-pre

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<header>
+  <h1>Diary</h1>
+  <nav>
+    <a href="/">home</a>
+    <a href="/read">read</a>
+    <a href="/write">write</a>
+    <a href="/edit">edit</a>
+  </nav>
+</header>
+
+<style lang='scss'>
+  header {
+    padding: 20px;
+    background: var(--main-color);
+    color: var(--text-light);
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Header from '../lib/components/Header.svelte';
+</script>
+
+<Header />
+<slot />
+
+<!-- 여기의 CSS는 전역적으로 적용이 된다. -->
+<style lang='scss' global>
+  @import '../app.scss';
+</style>
