a58c5045aae33766fd42fc09c5082b1929f2aadf
csslick <digitone@naver.com>
2024-05-26 20:33:39 +0900

    headar 영역 ui 동적 표시

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,17 +1,55 @@
+<script>
+  import { page } from '$app/stores';
+  import { goto } from '$app/navigation';
+
+  $: console.log('page', $page.url.pathname);
+
+  function clickHandler() {
+    if ($page.url.pathname === '/read') {
+      goto('/');
+    } else if ($page.url.pathname === '/write') {
+      // 쓰기 로직 추가
+      goto('/');
+    } else if ($page.url.pathname === '/edit') {
+      // 수정 로직 추가
+      goto('/');
+    }
+  }
+</script>
+
 <header>
-  <h1>Diary</h1>
-  <nav>
-    <a href="/">home</a>
-    <a href="/read">read</a>
-    <a href="/write">write</a>
-    <a href="/edit">edit</a>
-  </nav>
+  <h1>Diary {$page.url.pathname}</h1>
+  {#if $page.url.pathname === '/read'}
+    <button class='btn' on:click={() => goto('/')}>완료</button>
+  {:else if $page.url.pathname === '/write' } 
+    <button class='btn' on:click={() => goto('/')}>완료</button>
+  {:else if $page.url.pathname === '/edit' } 
+    <button class='btn' on:click={() => goto('/')}>완료</button>
+  {/if}
 </header>
+<nav>
+  <a href="/">home</a>
+  <a href="/read">read</a>
+  <a href="/write">write</a>
+  <a href="/edit">edit</a>
+</nav>
 
 <style lang='scss'>
   header {
+    display: flex;
+    justify-content: space-between;
+
+    h1 { font-size: 28px; }
     padding: 20px;
     background: var(--main-color);
     color: var(--text-light);
+
+    .btn {
+      border: none;
+      background: transparent;
+      color: var(--text-light);
+      font-size: 18px;
+      cursor: pointer;
+    }
   }
 </style>
