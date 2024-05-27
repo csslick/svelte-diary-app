e8eed778220f92fd0282fc77b85bff4c263c5733
csslick <digitone@naver.com>
2024-05-27 23:30:28 +0900

    textarea 자동높이 조절

--- src/routes/write/+page.svelte
+++ src/routes/write/+page.svelte
@@ -1,2 +1,35 @@
+<script>
+  import TextAreaAutoSize from '$lib/components/TextAreaAutoSize.svelte';
+  import { diaries } from '$lib/store/store';
 
-<h1>쓰기</h1>
+  let val = '';
+</script>
+
+<main>
+  <div class="diary">
+    <TextAreaAutoSize
+      bind:value={val}
+    ></TextAreaAutoSize>
+    <div class="bottom-info">
+      <span 
+        class="date">
+          { 
+            new Date().toLocaleDateString('ko-KR', {
+              year: 'numeric',
+              month: 'long',  // 'numeric' or 'long'
+              day: 'numeric',
+              weekday: 'long'
+            })
+          }
+        </span>
+    </div>
+  </div>
+</main>
+
+<style lang='scss'>
+  main { 
+    padding: 20px;
+    .diary { cursor: auto; }
+    .bottom-info { margin-top: 30px; }
+  }
+</style>
