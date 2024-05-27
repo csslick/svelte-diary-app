9db65dfecb679ac1bf44cd202049a38890619e48
csslick <digitone@naver.com>
2024-05-27 21:48:41 +0900

    home 글쓰기 버튼 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import addIcon from '$lib/assets/icon_add.svg';
   import Icon from '@iconify/svelte';
   import { diaries } from '$lib/store/store';
   import { goto } from '$app/navigation'; // 페이지 이동 모듈
@@ -26,3 +27,20 @@
   {/each}
 </main>
 
+<a href="/write" class='btn-write'>
+  <img src={addIcon} alt="add">
+</a>
+
+<style lang="scss">
+  .btn-write {
+    position: fixed;
+    bottom: 40px;
+    left: 50%;
+    transform: translateX(-50%);
+    cursor: pointer;
+    img {
+      width: 64px;
+      height: 64px;
+    }
+  }
+</style>
