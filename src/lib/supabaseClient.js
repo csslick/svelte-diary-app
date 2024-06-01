45a83a73c53c1e853ce2e00da2c5836ad83ac967
csslick <digitone@naver.com>
2024-06-02 00:29:31 +0900

    db data query & api key 정보 제거

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,6 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const projectURL = import.meta.env.VITE_DB_URL
 const anonKey = import.meta.env.VITE_DB_KEY
-console.log('supabase', projectURL, anonKey)
 
 export const supabase = createClient(projectURL, anonKey)
